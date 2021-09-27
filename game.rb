80cb38c3078eda50a0b1820a09cdb09c7a47f225
geocodinglife <geocodinglife@gmail.com>
2021-09-27 11:52:30 -0500

    test pass

--- game.rb
+++ game.rb
@@ -1,11 +1,47 @@
 require 'rspec'
+require 'awesome_print'
 
 class RPS
   attr_writer :rand_seed
+
+  def initialize(guess:)
+    @guess = guess.downcase
+  end
+
+  def randomization_algorithm
+    rand 2342343
+  end
+
+  def computer_guess
+    srand (@rand_seed) || randomization_algorithm
+    computer_guess = %w(rock paper scissors)
+    computer_guess.sample
+  end
+
+  def winner_is
+    if rule_engine[computer_guess.to_sym].include? @guess
+      'Computer wins'
+    elsif rule_engine[@guess.to_sym].include? computer_guess
+      'You win!'
+    else
+      'Tie'
+    end
+  end
+
+  def rule_engine
+    {
+      'rock':     ['scissors'],
+      'paper':    ['rock'],
+      'scissors': ['paper']
+    }
+  end
 end
 
+# ap rps = RPS.new(guess: 'Rock')
+# ap rps.winner_is
 
 describe RPS do
+
   it 'paper beats rock' do
     rps = RPS.new(guess: 'rock')
     rps.rand_seed = 1
